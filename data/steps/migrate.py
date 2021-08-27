b21e352b4dcb397f832a938b9cc2d5f0741a31f6
Adam Peter Banham <n7176546@qut.edu.au>
2021-08-27 14:17:14 +1000

    added migrate script and some basic checks

---
+++ data/steps/migrate.py
@@ -0,0 +1,60 @@
+import boto3
+import subprocess
+import json
+import hashlib
+import sys 
+
+OUTPUT_PATH = "data/out/copy_out.json"
+
+if __name__ == "__main__":
+    # get user input
+    print("Beginning Migration...")
+    bucket = input("Target Bucket Name :")
+    region = input("Target Region :")
+    location = f"s3://{bucket}/data/"
+    print(f"Using the following s3 location as a migration target: {location}")
+    #check bucket exits
+    exists = False
+    s3 = boto3.client('s3')
+    try :
+        s3.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={
+                'LocationConstraint' : region
+            }
+        )
+        exists = True
+    except s3.exceptions.BucketAlreadyExists:
+        exists = True
+    except s3.exceptions.BucketAlreadyOwnedByYou:
+        exists = True
+    except s3.exceptions.ClientError as e:
+        print("Unable to check if a bucket exits, likely an issue with AWS permission for your account or input.")
+        raise(e)
+    # start migrate
+    if (exists):
+        push = subprocess.call([ 
+            "aws",
+            "s3",
+            "sync",
+            "s3://mimic-iii-physionet",
+            location,
+            "--source-region",
+            "us-east-1",
+            "--dryrun"
+        ])
+
+        print(f"Mirgation completed :: {push}")
+        print("Saving location...")
+        # save out user input
+        out = {
+            'location' : location
+        }
+        # add hash
+        hash = hashlib.md5(json.dumps(out).encode()).hexdigest()
+        out['hash'] = hash 
+        # save output
+        json.dump(out,open(OUTPUT_PATH,'w'))
+    else:
+        print("unable to find migration target, stopping process...")
+    print("Finished...")
