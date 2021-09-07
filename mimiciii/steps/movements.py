06ac844d1b9509574ae0c098998bd3a35595cae3
Adam Peter Banham <n7176546@qut.edu.au>
2021-09-07 10:55:23 +1000

    reduced redundant code

--- mimiciii/steps/movements.py
+++ mimiciii/steps/movements.py
@@ -1,12 +1,7 @@
 from tqdm import tqdm 
-import boto3 
-from time import sleep 
-from glob import glob 
 from os import remove as remove_file
-from os import path
 import pandas as pd
-from ults import check_hash_in_map,get_query_s3_location,update_query_dbname,BATCH_SIZE
-import json
+from ults import get_query_s3_location,update_query_dbname,BATCH_SIZE,run_query
 from more_itertools import chunked
 
 
@@ -17,39 +12,6 @@ MOVEMENTS_CONTROFLOW_SQL = "mimiciii/in/movements_controlflow.SQL"
 MOVEMENTS_LOG_CONTROLFLOW_OUT_DIR = "mimiciii/out/movements/"
 MOVEMENTS_LOG_PATIENTS_OUT = "patient_universe.csv"
 MOVEMENTS_LOG_CONTROLFLOW_OUT_CSV = "controlflow_events.csv"
-
-def run_query(sql,filename,res_bucket:str,res_out:str):
-    # connect to aws athena
-    client = boto3.client("athena")
-    # send request
-    tqdm.write("starting query...")
-    res = client.start_query_execution(
-        QueryString=sql,
-        ResultConfiguration={
-            'OutputLocation' : f"s3://{res_bucket}/{res_out}"
-        }
-    )
-    # wait for completion
-    id = res['QueryExecutionId']
-    status = client.get_query_execution(
-        QueryExecutionId=id
-    )['QueryExecution']['Status']['State']
-    while status not in ['SUCCEEDED','FAILED','CANCELLED']:
-        tqdm.write(f"waiting for query to complete ({status})...")
-        sleep(2.5)
-        status = client.get_query_execution(
-            QueryExecutionId=id
-        )['QueryExecution']['Status']['State']
-    tqdm.write(f"query final state was :: {status}")
-    # collect result, copy to local, remove from remote
-    if status == 'SUCCEEDED':
-        tqdm.write("downloading query...")
-        s3 = boto3.resource("s3")
-        s3.meta.client.download_file(res_bucket,f'{res_out}/{id}.csv',f'{filename}')
-        return pd.read_csv(f"{filename}")
-    else:
-        raise SystemError(f"query failed :: {status}")
-        return []
 
 if __name__ == "__main__": 
     # find bucket and temp folder
